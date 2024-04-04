b98e456f2c8fbd2921f1477501a6d7772145978f
Beatriz <143760674+btrzslv@users.noreply.github.com>
2024-04-04 15:52:24 -0300

    desafio2024

---
+++ desafio2024/src/Components/Início.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Inicio() {
+  return (
+    <S.Inicio>
+      <img src={Perfil} alt="uma foto minha" />
+      
+      <h2>Olá! Meu nome é Beatriz</h2>
+
+    </S.Inicio>
+  )
+}
+
+export default Inicio
